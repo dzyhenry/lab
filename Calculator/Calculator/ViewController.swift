64a775bf9c818947d889c768feec4f32b6800652
dzyhenry <dzyhenry@foxmail.com>
2018-05-17 17:59:18 +0800

    add swift Calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by dzyhenry on 2018/5/16.
+//  Copyright © 2018年 dzyhenry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func appendDigit(_ sender: UIButton) {
+    }
+    
+
+}
+
