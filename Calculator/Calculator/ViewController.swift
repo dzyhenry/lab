be01e9d49bc8b6e12169a894619bf51a5c8e2f08
duzhaoyang <duzhaoyang@meituan.com>
2018-06-01 09:05:12 +0800

    "add enter"

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
+    var isInTheMiddleOfTypingNumber = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func appendDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isInTheMiddleOfTypingNumber {
+            resultLabel.text = resultLabel.text! + digit
+        } else {
+            resultLabel.text = digit
+            isInTheMiddleOfTypingNumber = true
+        }
+    }
+    @IBAction func enter(_ sender: UIButton) {
+        isInTheMiddleOfTypingNumber = false
     }
     
 
